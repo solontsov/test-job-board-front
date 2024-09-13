40d61b1613f7d75689f4c420ae0e3db66836de97
Oleksandr Solontsov <solontsov@gmail.com>
2024-09-13 13:22:15 +0100

    candidate interface

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Apply from './pages/apply';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<App />} />
+     <Route path="apply" element={<Apply />} />
+   </Routes>
+   </BrowserRouter>
+ </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
